c6df8a1f880b49d1dde24bb10974dc1cc04eff3e
jani <jani.karvo@fi.experis.com>
2022-09-13 14:38:36 +0300

    FEAT: buy laptop method

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const laptopPreviewPrice = document.getElementById("previewPrice");
 let laptops = [];
 payAmount.innerText = `0 €`;
 bankBalance.innerText = `0 €`;
-loanBalance.innerText = 0;
+loanBalance.innerText = `0 €`;
 
 try {
     fetch(`https://noroff-komputer-store-api.herokuapp.com/computers`)
@@ -22,10 +22,12 @@ try {
     console.log(error);
 };
 
+// call addLaptopSelection for every laptop fetched from komputer store api
 const updateLaptopSelection = (data) => {
     data.forEach(laptop => addLaptopToSelection(laptop))
 };
 
+// Add laptop to drop selection
 const addLaptopToSelection = (laptop) => {
     const laptopElement = document.createElement("option");
     laptopElement.value = laptop.id;
@@ -33,6 +35,7 @@ const addLaptopToSelection = (laptop) => {
     laptopSelection.appendChild(laptopElement);
 };
 
+// Sets laptop preview elements according to current selection of laptop
 const setLaptopPreview = e =>{
     const selected = laptops[e.target.selectedIndex];
     laptopFeatures.innerText = selected.description;
@@ -54,14 +57,15 @@ const doWork = () => {
 const bankMoney = () => {
     let pay = parseInt(payAmount.innerText);
     let bank = parseInt(bankBalance.innerText);
-    let loan = parseInt(loanBalance.innerText);
+    let loan = parseInt(loanBalance.innerText.split(" ")[0]);
     if(loan > 0){
-        if(pay * 0.1 > loan){
+        if(pay * 0.1 >= loan){
            pay -= loan;
            loanBalance.innerText = 0;
-           document.getElementById("repay").style.display = "none" 
+           changeVisibility(false) 
         }else{
-            loanBalance.innerText -= (pay*0.1);
+            loan -= (pay*0.1);
+            loanBalance.innerText = `${loan} €`;            
             pay -= (pay*0.1); 
         }
     }    
@@ -77,29 +81,57 @@ const bankMoney = () => {
 // Pops up a prompt for user to enter the loan amount wanted.
 // Loan amount cannot exceed bank balance * 2
 const takeLoan = () => {
-
-
-    document.getElementById("repay").style.display = "flex"
-    document.getElementById("loanAmount").style.display = "flex"
-    document.getElementById("loanButton").style.display = "none"
+    let bank = parseInt(bankBalance.innerText.split(" ")[0]);
+    const loan = parseInt(prompt("Enter the amount you want to loan: "));
+    if(loan != null && loan <= bank * 2){        
+        loanBalance.innerText = `${loan} €`
+        bankBalance.innerText = `${bank+loan} €`
+        changeVisibility(true)
+    }
+    
 };
 
 // Use all of pay amount to reduce loan
 const repayLoan = () => {
-    let pay = parseInt(payAmount.innerText);
-    let loan = parseInt(loanBalance.innerText);
+    let pay = parseInt(payAmount.innerText.split(" ")[0]);
+    let loan = parseInt(loanBalance.innerText.split(" ")[0]);
     
-    if(pay > loan){
+    if(pay >= loan){
         pay -= loan;
         loanBalance.innerText = 0;
         payAmount.innerText = `${pay} €`;
+        changeVisibility(false)
+    }else if(pay > 0){
+        loan -= pay;
+        loanBalance.innerText = `${loan} €`; 
+        payAmount.innerText = `${pay-pay} €`; 
+    }    
+};
+
+// Use bank balance to buy a computer
+const buyLaptop = () => {
+    let totalMoneys = parseInt(bankBalance.innerText.split(" ")[0]);
+    let price = parseInt(laptopPreviewPrice.innerText.split(" ")[0]);    
+    if(totalMoneys >= price){
+        bankBalance.innerText = `${totalMoneys-price} €`;
+        alert(`You have bought a almost brand new calculation machine`)
+    }else{
+        alert(`Insufficient funds! You are ${price - totalMoneys}€ short. Go to work or take more loan to buy this state of art machine.`)
+    }
+};
+
+// Takes booolean as a param, if true => sets repay loan button and loan amount field visible
+// and take loan button hidden and vice versa
+const changeVisibility = (isLoan) =>{
+    if(isLoan){
+        document.getElementById("repay").style.display = "flex"
+        document.getElementById("loanAmount").style.display = "flex"
+        document.getElementById("loanButton").style.display = "none"
+    }else{
         document.getElementById("repay").style.display = "none"
         document.getElementById("loanAmount").style.display = "none"
         document.getElementById("loanButton").style.display = "flex"
-    }else{
-        loanBalance.innerText -= pay;
-        payAmount.innerText = `${pay-pay} €`; 
-    }    
+    }
 };
 
 // Event listeners
